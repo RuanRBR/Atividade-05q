32590c4e27dbe71f86b60ab44a08f9cf66f4e047
RUAN BRÖERING RODRIGUES <ruan_rodrigues3@estudante.sesisenai.org.br>
2025-08-14 08:31:11 -0300

    teste

--- att05/src/App.jsx
+++ att05/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import TituloPagina from './TituloPagina';
+import DetalhesProduto from './DetalhesProduto';
 
 const App = () => {
+    const produtoA = {
+        nome: 'Notebook',
+        preco: 3500.00,
+        descricao: 'Notebook de última geração com 16GB de RAM e SSD de 512GB.'
+    };
+
     return (
         <>
             <TituloPagina />
+            <DetalhesProduto produto={produtoA} />
         </>
     );
 };
