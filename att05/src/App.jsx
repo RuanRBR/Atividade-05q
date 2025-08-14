d57b9f902df04cd80d3feebd73fad0edf74f2fec
RUAN BRÖERING RODRIGUES <ruan_rodrigues3@estudante.sesisenai.org.br>
2025-08-14 08:29:31 -0300

    Aplicação

---
+++ att05/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TituloPagina from './TituloPagina';
+
+const App = () => {
+    return (
+        <>
+            <TituloPagina />
+        </>
+    );
+};
+
+export default App;
---
+++ att05/src/TituloPagina.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TituloPagina = () => {
+    return <h1>Minha Página em React</h1>;
+};
+
+export default TituloPagina;
